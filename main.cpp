8f283da84cb71872e3466102be6de2fc853c1da7
JF <jasonhfields@gmail.com>
2015-04-06 21:50:47 -0500

    Centered window

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ bool init()
   else
   {
     // Create window
-    gWindow = SDL_CreateWindow( gTitle, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    gWindow = SDL_CreateWindow( gTitle, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
     if( gWindow == NULL )
     {
       printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
