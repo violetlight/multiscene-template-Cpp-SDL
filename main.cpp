74763be64705420f6426cbb6cbc98da9c6d8f6a8
JF <jasonhfields@gmail.com>
2015-04-06 20:41:55 -0500

    Tabs->spaces, does it help?

--- main.cpp
+++ main.cpp
@@ -15,42 +15,42 @@ SDL_Surface* gScreenSurface = NULL;
 
 bool init()
 {
-	bool success = true;
+  bool success = true;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface( gWindow );
-		}
-	}
+  //Initialize SDL
+  if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+  {
+    printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+    success = false;
+  }
+  else
+  {
+    //Create window
+    gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( gWindow == NULL )
+    {
+      printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+      success = false;
+    }
+    else
+    {
+      //Get window surface
+      gScreenSurface = SDL_GetWindowSurface( gWindow );
+    }
+  }
 
-	return success;
+  return success;
 }
 
 void close()
 {
 
-	//Destroy window
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
+  //Destroy window
+  SDL_DestroyWindow( gWindow );
+  gWindow = NULL;
 
-	//Quit SDL subsystems
-	SDL_Quit();
+  //Quit SDL subsystems
+  SDL_Quit();
 }
 
 int main ( int argc, char* args[] )
