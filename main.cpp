c3ebae46986b81d170e3af81a84c57ec1ff1d6a0
JF <jasonhfields@gmail.com>
2015-04-06 20:34:42 -0500

    init and close functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,76 @@
 #include <sdl2/SDL.h>
+#include <stdio.h>
+#include <string>
 
-int main ()
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+bool init();
+void close();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+
+
+bool init()
 {
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+		}
+	}
+
+	return success;
+}
+
+void close()
+{
+
+	//Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+int main ( int argc, char* args[] )
+{
+
+
+  if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+
+  printf( "ESS DEE ELL\n" );
+  close();
+
+  // pg.init(), screen, screen rect, etc..
+
+  // control object
+  // std::map (dict) of states
+  // control object init, pass in states dict
+  // control object main loop()
+
   return 0;
 }
