a3c28b25ce3f7974c73e1242ecd6d352313b339b
JF <jasonhfields@gmail.com>
2015-05-04 20:04:59 -0500

    more nullptr

--- main.cpp
+++ main.cpp
@@ -73,7 +73,7 @@ SDL_Surface* loadSurface( std::string path )
 {
   //Load image at specified path
   SDL_Surface* loadedSurface = SDL_LoadBMP( path.c_str() );
-  if( loadedSurface == NULL )
+  if( loadedSurface == nullptr )
   {
     printf( "Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
   }
