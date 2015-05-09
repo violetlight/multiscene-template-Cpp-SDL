a919eb23f87151df55f8ea447ba5119a7ad79ca2
JF <jasonhfields@gmail.com>
2015-05-09 09:09:27 -0500

    Directly reference maps in close function of main.cpp

--- main.cpp
+++ main.cpp
@@ -10,27 +10,25 @@
 #include "data/sprite.h"
 #include <iostream>
 
-void close(std::map<std::string, SDL_Texture*> gfx,
-           std::map<std::string, Mix_Music*> music,
-           std::map<std::string, TTF_Font*> fonts)
+void close()
 {
 
   // Destroy gfx
-  for (auto &img: gfx)
+  for (auto &img: Tools::gfx)
   {
     SDL_DestroyTexture(img.second);
     img.second = nullptr;
   }
 
   // Destroy music
-  for (auto &track: music)
+  for (auto &track: Tools::music)
   {
     Mix_FreeMusic(track.second);
     track.second = nullptr;
   }
 
   // Destroy fonts
-  for (auto &font: fonts)
+  for (auto &font: Tools::fonts)
   {
     TTF_CloseFont(font.second);
     font.second = nullptr;
@@ -55,26 +53,18 @@ int main (int argc, char* args[])
 {
 
   Prepare::init();
+  Tools::loadMedia();
 
-  // all of this needs to go into a loadMedia function or something
-  std::map<std::string, Mix_Music*> music;
-  Tools::loadMusic(music);
-
-  std::map<std::string, SDL_Texture*> gfx;
-  Tools::loadGraphics(gfx);
-
-  std::map<std::string, TTF_Font*> fonts;
-  Tools::loadFonts(fonts);
 
   Tools::Sprite testText;
   SDL_Color textColor = {0x20,0x0F,0xA0};
-  if (!testText.setImageFromText("helloooooooooo world", textColor, fonts["Fixedsys500c.ttf"]))
+  if (!testText.setImageFromText("helloooooooooo world", textColor, Tools::fonts["Fixedsys500c.ttf"]))
   {
     printf("Failed to render text!\n");
   }
 
   Tools::Sprite testGirl;
-  if( !testGirl.setImage( gfx["sarn.png"] ) )
+  if( !testGirl.setImage( Tools::gfx["sarn.png"] ) )
   {
     printf( "Failed to load Foo' texture image!\n" );
   }
@@ -84,7 +74,7 @@ int main (int argc, char* args[])
   int WALKING_ANIMATION_FRAMES = 4;
   SDL_Rect walkingClips[WALKING_ANIMATION_FRAMES];
   Tools::Sprite walkingSpriteSheet;
-  if( !walkingSpriteSheet.setImage( gfx["foo.png"] ) )
+  if( !walkingSpriteSheet.setImage( Tools::gfx["foo.png"] ) )
   {
     printf( "Failed to load Foo' texture image!\n" );
   }
@@ -111,7 +101,6 @@ int main (int argc, char* args[])
     walkingClips[ 3 ].w =  64;
     walkingClips[ 3 ].h = 205;
   }
-  // --------------------------------------------------------------
 
   bool quit = false;
   SDL_Event e;
@@ -134,7 +123,7 @@ int main (int argc, char* args[])
         {    // no music playing
           if (Mix_PlayingMusic() == 0)
           {    // then play it
-            if (Mix_PlayMusic(music["koorong.wav"], -1) == -1)
+            if (Mix_PlayMusic(Tools::music["koorong.wav"], -1) == -1)
             {  // error
               return 1;
             } // music is playing
@@ -192,7 +181,7 @@ int main (int argc, char* args[])
     SDL_RenderClear(Screen::renderer);
 
     // Render splash image
-    SDL_RenderCopy(Screen::renderer, gfx["splash2.png"], NULL, NULL );
+    SDL_RenderCopy(Screen::renderer, Tools::gfx["splash2.png"], NULL, NULL );
     testText.render( ( Constants::SCREEN_W - testText.getWidth() ) / 2, ( Constants::SCREEN_H - testText.getHeight() ) / 2 );
     testGirl.setAlpha(alpha);
     testGirl.render(0,0);
@@ -219,6 +208,6 @@ int main (int argc, char* args[])
   // control object init, pass in states dict
   // control object main loop()
 
-  close(gfx, music, fonts);
+  close();
   return 0;
 }
