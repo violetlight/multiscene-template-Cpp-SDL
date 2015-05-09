d9c39a0e6fc264f24daf26b432e19f184dc27ea3
JF <jasonhfields@gmail.com>
2015-05-09 09:09:58 -0500

    Control object framework

---
+++ data/control.h
@@ -0,0 +1,16 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+#include "constants.h"
+
+namespace Tools {
+  class Control
+  {
+    //self.screen
+    self.caption = Constants::CAPTION
+  public:
+    Control();
+    ~Control();
+  };
+}
+
+#endif
